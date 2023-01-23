e426e8b133e5f90187b8e6f7127804921cf95f94
not-Whale <rezepinn@yandex.ru>
2023-01-23 04:13:15 +0300

    Удалил закрытый TODO

--- src/main.py
+++ src/main.py
@@ -11,8 +11,6 @@ class App:
         self.root.title('rezepinn/bmstu/pendulum.app')
         self.root.resizable(False, False)
 
-        # TODO: добавить tag при размещении на canvas для удаления элементов
-        # TODO: для удаления использовать canvas.delete("tag")
         self.pendulum = pendulum.Pendulum(self.root)
         self.timer = timer.Timer(self.root, self.pendulum)
         self.settings = settings.Settings(self.root, self.pendulum)
